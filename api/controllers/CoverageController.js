aaeec5dddf96cfc2165bdd6d8aeb5fd4f2fdfc34
Corey White <smortopahri@gmail.com>
2015-02-15 23:23:34 -0500

    Added new api and module

---
+++ api/controllers/CoverageController.js
@@ -0,0 +1,11 @@
+/**
+ * CoverageController
+ *
+ * @description :: Server-side logic for managing coverages
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
