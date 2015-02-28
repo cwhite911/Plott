73765e4be48005df1eadd2e7246029a25817e674
Corey White <smortopahri@gmail.com>
2015-02-28 18:20:47 -0500

    Added trilateration

--- plott/api/controllers/TracksController.js
+++ plott/api/controllers/TracksController.js
@@ -4,8 +4,25 @@
  * @description :: Server-side logic for managing tracks
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
-
-
+var wifiscanner = require('node-wifiscanner'),
+		trilateration = require('trilateration'),
+		knownAPs = [
+			{
+				mac: '0a:18:d6:53:73:7c',
+				x: -78.902439,
+				y: 36.022025
+			},
+			{
+				mac: 'c8:d7:19:3b:db:ff',
+				x: -78.902483,
+				y: 36.022058
+			},
+			{
+				mac: '0a:18:d6:53:73:dc',
+				x: -78.902517,
+				y: 36.021989
+			}
+		];
 //Converts dBm to quality
 function signalQualityToPernect (dBm) {
 	var quality;
@@ -36,9 +53,46 @@ function signalToDistance (m, p){
 }
 
 module.exports = {
+	// var spotInfo;
 	signalToPoint: function (req, res){
-		var d1 = signalToDistace(80, 400),
-				d2 = signalToDistace(80, 400),
-				d3 = signalToDistace(80, 400);
+
+		wifiscanner.scan(function(err, data){
+			if (err) {
+				console.log("Error : " + err);
+				return;
+			}
+			// spotInfo = {
+			// 	timeStamp: +new Date(),
+			// 	data: data
+			// };
+			// Coverage.create(spotInfo).exec(function createCB(err,created){
+			// 	if (err){
+			// 		console.error(err);
+			// 	}
+			// 	console.log(created.data.length + ' access points collected.');
+			// });
+			var distance;
+			data.forEach(function(ap){
+				for (var i = 0, l = knownAPs.length; i < l; i++){
+					if (ap.mac === knownAPs[i].mac){
+						console.log(ap.mac);
+						//Add Beacon
+						trilateration.addBeacon(i, trilateration.vector(knownAPs[i].x, knownAPs[i].y));
+						distance = signalToDistance(ap.signal_level, 400);
+						//Setting the beacons distances
+						console.log({index: i, distance: distance});
+						trilateration.setDistance(i, distance);
+					}
+				}
+			});
+		});
+
+
+		// Start Calculation
+		var pos = trilateration.calculatePosition();
+
+		console.log("X: " + pos.x + "; Y: " + pos.y);
+
+				res.send(pos);
 	}
 };
