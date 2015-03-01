6ffc96eef6f424f80d088cf90256ce3e9fa2bc2c
Corey White <smortopahri@gmail.com>
2015-03-01 17:26:35 -0500

    trilateration is working, but variables need to be refined to improve accuacy, position is also being converted back to geojson wgs84

--- plott/api/controllers/TracksController.js
+++ plott/api/controllers/TracksController.js
@@ -6,21 +6,36 @@
  */
 var wifiscanner = require('node-wifiscanner'),
 		trilateration = require('trilateration'),
+		transform = require('proj4geojson'),
+		//EPSG:2264 proj4 http://spatialreference.org/ref/epsg/2264/
+		sr = '+proj=lcc +lat_1=36.16666666666666 +lat_2=34.33333333333334 +lat_0=33.75 +lon_0=-79 +x_0=609601.2192024384 +y_0=0 +ellps=GRS80 +datum=NAD83 +to_meter=0.3048006096012192 +no_defs';
 		knownAPs = [
 			{
+				name: 'Bates',
 				mac: '0a:18:d6:53:73:7c',
-				x: -78.902439,
-				y: 36.022025
+				// x: -78.902439,
+				// y: 36.022025
+				x: 2028837.603,
+				y: 826942.359,
+				range: 20
 			},
 			{
+				name: 'Chobey',
 				mac: 'c8:d7:19:3b:db:ff',
-				x: -78.902483,
-				y: 36.022058
+				// x: -78.902483,
+				// y: 36.022058
+				x: 2028850.515,
+				y: 826930.098,
+				range: 10
 			},
 			{
+				name:'Autumn',
 				mac: '0a:18:d6:53:73:dc',
-				x: -78.902517,
-				y: 36.021989
+				// x: -78.902517,
+				// y: 36.021989
+				x: 2028827.403,
+				y: 826917.403,
+				range: 20
 			}
 		];
 //Converts dBm to quality
@@ -55,7 +70,19 @@ function signalToDistance (m, p){
 module.exports = {
 	// var spotInfo;
 	signalToPoint: function (req, res){
-
+		var geojson = {
+  		"type": "FeatureCollection",
+  		"features": [
+    		{
+      		"type": "Feature",
+      		"properties": {},
+      		"geometry": {
+        		"type": "Point",
+        		"coordinates": []
+      		}
+    		}
+  		]
+		};
 		wifiscanner.scan(function(err, data){
 			if (err) {
 				console.log("Error : " + err);
@@ -71,14 +98,16 @@ module.exports = {
 			// 	}
 			// 	console.log(created.data.length + ' access points collected.');
 			// });
-			var distance;
+			var distance,
+			    pos,
+					wgs84geojson;
 			data.forEach(function(ap){
 				for (var i = 0, l = knownAPs.length; i < l; i++){
 					if (ap.mac === knownAPs[i].mac){
 						console.log(ap.mac);
 						//Add Beacon
 						trilateration.addBeacon(i, trilateration.vector(knownAPs[i].x, knownAPs[i].y));
-						distance = signalToDistance(ap.signal_level, 400);
+						distance = signalToDistance(ap.signal_level, knownAPs[i].range);
 						//Setting the beacons distances
 						console.log({index: i, distance: distance});
 						trilateration.setDistance(i, distance);
@@ -89,10 +118,15 @@ module.exports = {
 
 
 		// Start Calculation
-		var pos = trilateration.calculatePosition();
+		pos = trilateration.calculatePosition();
 
 		console.log("X: " + pos.x + "; Y: " + pos.y);
 
-				res.send(pos);
+		//Set coordinates in geojson
+		geojson.features[0].geometry.coordinates = [pos.x, pos.y];
+		//Converts geojson feature collection coordinates from SRID:2264 to SRID: 4326
+		wgs84geojson = transform.to(geojson, sr);
+		//Sends the response geojson
+		res.json(wgs84geojson);
 	}
 };
