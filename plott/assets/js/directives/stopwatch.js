c4a2b55986c9a4263298aa9785ae3662b3a7d38c
Corey White <smortopahri@gmail.com>
2015-03-14 16:04:39 -0400

    Moved stopwatch to factory

--- plott/assets/js/directives/stopwatch.js
+++ plott/assets/js/directives/stopwatch.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('plott')
-.directive('stopWatch', ['$interval', '$filter' , function ($interval, $filter) {
+.directive('stopWatch', function(StopWatchFactory) {
   return {
     restrict: 'E',
     transclude: true,
@@ -10,83 +10,8 @@ angular.module('plott')
     // },
     templateUrl: 'templates/stop-watch.html',
     link: function (scope, element) {
+      scope.stopwatch = new StopWatchFactory();
 
-      scope.time = {
-        hour: 0,
-        min: 0,
-        sec: 0,
-        mili: 0,
-        timeArray: ['hour', 'min', 'sec', 'mili'],
-        current: '00:00:00:00',
-        display: function (){
-          scope.time.timeArray.forEach(function(item){
-            scope.time[item] = parseInt(scope.time[item], 10);
-            if (scope.time[item] < 10){
-              scope.time[item] = 0 + scope.time[item].toString();
-            }
-          });
-
-          // scope.time.min = parseInt(scope.time.min, 10);
-          // scope.time.sec = parseInt(scope.time.sec, 10);
-          //
-          //
-          // if (scope.time.min < 10){
-          //   scope.time.min = 0 + scope.time.min.toString();
-          // }
-          // if (scope.time.hour < 10){
-          //   scope.time.hour = 0 + scope.time.hour.toString();
-          // }
-
-
-          // d.setHours(scope.time.hour,scope.time.min,scope.time.sec);
-          // return $filter('date')(d, 'h:mm:ss');
-          return scope.time.hour + ':' + scope.time.min + ':' + scope.time.sec + ':' + scope.time.mili;
-        }
-      };
-
-      //Start timer
-      scope.start = function(){
-        //Create interval set the stopHelper variable to the interval promise so that it can be used in the stop function.
-        scope.stopHelper =  $interval(function(){
-          if (scope.time.mili < 100){
-            scope.time.mili++;
-          }
-          else if (scope.time.mili === 100){
-            scope.time.sec++;
-            scope.time.mili = '00';
-          }
-          // if (scope.time.sec < 60){
-          //   scope.time.sec++;
-          // }
-          if (scope.time.sec === 60){
-            scope.time.min++;
-            scope.time.sec = '00';
-          }
-          if (scope.time.min === 60){
-            scope.time.hour++;
-            scope.time.min = '00';
-          }
-          scope.time.current = scope.time.display();
-        }, 10);
-      };
-
-      //Stop timer
-      scope.stop = function (){
-        if (angular.isDefined(scope.stopHelper)) {
-          $interval.cancel(scope.stopHelper);
-          scope.stopHelper = undefined;
-        }
-      };
-
-      //Reset timer
-      scope.reset = function (){
-        angular.extend(scope.time, {
-          hour: 0,
-          min: 0,
-          sec: 0,
-          current: '00:00:00:00'
-        });
-      };
-    }
   }
-}]);
+  }
+});
---
+++ plott/assets/js/factories/stopwatch.js
@@ -0,0 +1,82 @@
+'use strict';
+
+angular.module('plott')
+.factory('StopWatchFactory', function($interval){
+  //Private
+  var stopHelper;
+
+  var Time = function(){
+    this.hour = 0;
+    this.min = 0;
+    this.sec = 0;
+    this.mili = 0;
+    this.timeArray = ['hour', 'min', 'sec', 'mili'];
+    this.current = '00:00:00:00';
+    return this;
+  };
+
+  Time.prototype = {
+    display: function (){
+      var that = this;
+      this.timeArray.forEach(function(item){
+        that[item] = parseInt(that[item], 10);
+        if (that[item] < 10){
+          that[item] = 0 + that[item].toString();
+        }
+      });
+
+      return this.hour + ':' + this.min + ':' + this.sec + ':' + this.mili;
+    },
+
+  //Start timer
+  start: function(){
+    var that = this;
+    //Create interval set the stopHelper variable to the interval promise so that it can be used in the stop function.
+    stopHelper =  $interval(function(){
+      if (that.mili < 100){
+        that.mili++;
+      }
+      else if (that.mili === 100){
+        that.sec++;
+        that.mili = '00';
+      }
+      // if (scope.time.sec < 60){
+      //   scope.time.sec++;
+      // }
+      if (that.sec === 60){
+        that.min++;
+        that.sec = '00';
+      }
+      if (that.min === 60){
+        that.hour++;
+        that.min = '00';
+      }
+      that.current = that.display();
+    }, 10);
+    return this;
+  },
+
+  //Stop timer
+  stop: function (){
+    if (angular.isDefined(stopHelper)) {
+      $interval.cancel(stopHelper);
+      stopHelper = undefined;
+    }
+    return this;
+  },
+
+  //Reset timer
+  reset: function (){
+    angular.extend(this, {
+      hour: 0,
+      min: 0,
+      sec: 0,
+      current: '00:00:00:00'
+    });
+    return this;
+  }
+};
+
+return (Time);
+
+});
